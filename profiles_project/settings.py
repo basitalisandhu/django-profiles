d685dfab0c2ab22de9a0dfa99bcd62025afd7f9e
Basit Ali Sandhu <muhammadbasitali0@gmail.com>
2021-04-25 03:08:22 +0500

    [updated latest]

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_api',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +72,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'profiles_project.wsgi.application'
-
+REST_FRAMEWORK = {
+      'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+      }
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
